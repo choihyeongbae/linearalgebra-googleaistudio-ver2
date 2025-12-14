aa3e3f467f6e7d8f2ea636367b2cd190f0e1726e
choihyeongbae <149979597+choihyeongbae@users.noreply.github.com>
2025-12-14 23:00:27 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
 import path from 'path';
-import { defineConfig, loadEnv } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+export default defineConfig({
+  plugins: [react()],
+  base: '/la_gasver/', // GitHub 저장소 이름으로 변경
+})
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
